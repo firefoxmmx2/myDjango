64dc24649eb42ceb99ffb26013288836d656dcd2
firefoxmmx2 <firefoxmmx@gmail.com>
2012-08-06 16:17:23 +0800

    添加OS模块

--- myDjango/settings.py
+++ myDjango/settings.py
@@ -12,12 +12,13 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'ENGINE':'django.db.backends.sqlite3',
+#        'ENGINE': 'django.db.backends.oracle', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': '/home/hooxin/Work/myDjango/sqlite.db',                      # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+#        'USER': 'mansys_jdy',                      # Not used with sqlite3.
+#        'PASSWORD': 'mansys_jdy',                  # Not used with sqlite3.
+#        'HOST': '192.168.1.202',                      # Set to empty string for localhost. Not used with sqlite3.
+#        'PORT': '1521',                      # Set to empty string for default. Not used with sqlite3.
     }
 }
 
@@ -88,7 +89,7 @@ SECRET_KEY = '88nyjrfu=bgkpv5bp_qn$qsx+z_s%z5oq2udnun(u9eo!za6h+'
 TEMPLATE_LOADERS = (
     'django.template.loaders.filesystem.Loader',
     'django.template.loaders.app_directories.Loader',
-#     'django.template.loaders.eggs.Loader',
+     'django.template.loaders.eggs.Loader',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -105,6 +106,8 @@ ROOT_URLCONF = 'myDjango.urls'
 
 # Python dotted path to the WSGI application used by Django's runserver.
 WSGI_APPLICATION = 'myDjango.wsgi.application'
+
+import os
 
 TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
